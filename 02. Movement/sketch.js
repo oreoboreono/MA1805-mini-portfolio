794c15dfe1b9b59d193423bd460efa782199e872
OreoB <oreoboreon@gmail.com>
2025-10-13 16:56:33 +0100

    c

--- 02. Movement/sketch.js
+++ 02. Movement/sketch.js
@@ -1,7 +1,45 @@
+let x;
+let y;
 function setup() {
-  createCanvas(400, 400);
+  
+  createCanvas(500, 500);
 }
 
+function scream(){
+  x = random(-30,120);
+  y = random(200,200);
+
+  for(let i = 0; i<10; i++){
+  fill(255,0,0);
+  textStyle(BOLD)
+  textSize(50);
+  text("LET ME SCREAM", x,y+(i*50))
+  }
+
+
+}
+
+function person(){
+  //face
+   push();
+   fill(0,255,0);
+   noStroke();
+  ellipse(width/2,height/2+10,130,170);
+  pop();
+
+  //body
+  fill(0,255,0)
+  noStroke();
+  rect(width/2 -120, (height/2) +160, 250, 350);
+  ellipse(width/2 -120, (height/2) +230,130,170);
+  ellipse(width/2 +120 , (height/2) +230,130,170);
+
+}
+
+
 function draw() {
-  background(220);
+  background(0);
+  scream();
+  person();
+
 }
