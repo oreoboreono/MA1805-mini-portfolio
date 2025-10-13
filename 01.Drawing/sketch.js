4350460280cf912fd02b798806f0413fd75b4e00
OreoB <oreoboreon@gmail.com>
2025-10-13 14:13:11 +0100

    c

--- 01.Drawing/sketch.js
+++ 01.Drawing/sketch.js
@@ -1,13 +1,29 @@
-function setup() {
-  createCanvas(400, 400);
+let img;
+function eye(x, y, size) { 
+  translate(x, y);
+  noStroke(); 
+  fill(255)
+  triangle(-size, 0, 0, -size, 0, size);
+  triangle(size, 0, 0, -size, 0, size);
+  fill(0);
+  rectMode(CENTER);
+  rect(0, 0, size * 0.09, size); 
 }
 
-function draw() {
-  background(220);
-  stroke(0,0,153)
-  strokeWeight(2)
-  fill(0,128,255)
-  circle(100,100,100);
-  
-
+function setup() {
+  grainImg = loadImage("C:\Users\Oreo\Downloads\grains.png");
+  createCanvas(400, 400);
+  background(30);
+  fill(0);
+  stroke(255);
+  strokeWeight(2);
+  circle(195, 140, 260);
+  eye(120, 100, 40);
+  eye(150, 0, 40)
+  fill(255);
+  textStyle(BOLD)
+  textSize(32)
+  text("I have no mouth", -200, 230);
+  text("I must scream", -190, 270);
+  image(grainImg, 0, 0);
 }
